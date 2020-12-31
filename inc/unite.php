1e430d48ac87e82e8b49612c01ab05f9c37cc35a
mohamed elshafiy <blackzetso>
2020-12-31 04:58:43 +0200

    fix cart

--- inc/unite.php
+++ inc/unite.php
@@ -19,39 +19,11 @@ if(isset($_GET["unite"]) && !empty($_GET["unite"])){
     $row = $query->fetch(); ?>
     <input type="text" class="salary-item" value="<?php echo $row['price2']; ?>" disabled> ج
 <?php } } ?>
-<script> 
-        
-        // get element in make function plus ab reducse
-        let Weight = document.querySelector(".Weight-value"),
-        btnPlus = document.querySelector(".btn-plus"),
-        btnReduce = document.querySelector(".btn-reduce");
-        let i = 1;
 
-        Weight.value = 1;
-        btnPlus.onclick = ()=>{plus()}
-        btnReduce.onclick = ()=>{reduce()}
+<script>
+    //get element use in calculate Weight
+    salaryItem = document.querySelector(".salary-item"),
+    salaryAll = document.querySelector(".salary-all");
 
-        const plus = ()=>{
-            Weight.value = i = i + <?php echo $row['Decimal_number']; ?>;
-            calculate()
-        }
-        const reduce = ()=>{
-            if(Weight.value <= <?php echo $row['Decimal_number']; ?>){
-            }
-            else{
-                Weight.value = i = i - <?php echo $row['Decimal_number']; ?>;
-                calculate()
-            }
-        }
-        //get element use in calculate Weight
-        let salaryItem = document.querySelector(".salary-item"),
-        salaryAll = document.querySelector(".salary-all");
-
-
-
-        //calculate Weight
-        const calculate = ()=>{ 
-            salaryAll.value = salaryItem.value * Weight.value
-        }
-        calculate();  
+    salaryAll.value = salaryItem.value * Weight.value
 </script>
